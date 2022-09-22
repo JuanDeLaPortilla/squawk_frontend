3f063279671fe6267561075f24f1ac4cbe146988
Juan De La Portilla <juandelaportillacardenas@gmail.com>
2022-09-22 12:27:52 -0500

    added localstorage dark theme and changed about-us page

--- js/a-tags.js
+++ js/a-tags.js
@@ -1,7 +1,6 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-const themeToggler = document.querySelector(".theme-toggler");
 
 // show sidebar
 menuBtn.addEventListener('click', () => {
@@ -11,15 +10,6 @@ menuBtn.addEventListener('click', () => {
 // close sidebar
 closeBtn.addEventListener('click', () =>{
     sideMenu.style.display = 'none';
-})
-
-// change theme
-themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
-
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-
 })
 
 //fill users in table
--- js/white-theme.js
+++ js/white-theme.js
@@ -1,6 +1,24 @@
-const themeToggler = document.querySelector(".tema");
+const btnSwitch = document.querySelector('#switch');
 
-// change theme
-themeToggler.addEventListener('click', () => {
+btnSwitch.addEventListener('click', () => {
+
     document.body.classList.toggle('dark-theme-variables');
-})
+
+    btnSwitch.classList.toggle('active');
+
+    // Guardamos el modo en localstorage.
+    if (document.body.classList.contains('dark-theme-variables')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+// Obtenemos el modo actual.
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-theme-variables');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark-theme-variables');
+    btnSwitch.classList.remove('active');
+}
